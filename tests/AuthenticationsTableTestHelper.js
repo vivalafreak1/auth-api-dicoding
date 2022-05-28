abd83f48c956960c05fd7b0cd576fc6e6f0e7d52
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-05-28 15:35:12 +0700

    Initializing project

---
+++ tests/AuthenticationsTableTestHelper.js
@@ -0,0 +1,31 @@
+/* instanbul ignore file */
+/* eslint linebreak-style: ["error", "windows"] */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const AuthenticationsTableTestHelper = {
+  async addToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+
+    await pool.query(query);
+  },
+
+  async findToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    const result = await pool.query(query);
+
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('TRUNCATE TABLE authentications');
+  },
+};
+
+module.exports = AuthenticationsTableTestHelper;
