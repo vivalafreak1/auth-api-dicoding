0c6344e88acaa89d36749cc0f203796cf18912ee
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-05-29 22:17:42 +0700

    User Repository and PasswordHash implementation

---
+++ src/Infrastructures/security/BcryptPasswordHash.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint linebreak-style: ["error", "windows"] */
+const PasswordHash = require('../../Applications/security/PasswordHash');
+
+class BcryptPasswordHash extends PasswordHash {
+  constructor(bcrypt, saltRound = 10) {
+    super();
+    this._bcrypt = bcrypt;
+    this._saltRound = saltRound;
+  }
+
+  async hash(password) {
+    return this._bcrypt.hash(password, this._saltRound);
+  }
+}
+
+module.exports = BcryptPasswordHash;
