968961fc9b8a9e9acd1262789b1828fee32f417f
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-05-29 14:48:51 +0700

    Create AddUser Use Case and PasswordHash Interface

---
+++ src/Applications/security/PasswordHash.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint linebreak-style: ["error", "windows"] */
+
+class PasswordHash {
+  async hash(password) {
+    throw new Error('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = PasswordHash;
---
+++ src/Applications/security/_test/PasswordHash.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* eslint linebreak-style: ["error", "windows"] */
+
+const PasswordHash = require('../PasswordHash');
+
+describe('PasswordHash interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const passwordHash = new PasswordHash();
+
+    // Action & Assert
+    await expect(passwordHash.hash('dummy_password')).rejects.toThrowError('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+  });
+});
