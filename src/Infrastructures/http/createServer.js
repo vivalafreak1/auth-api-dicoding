c1bcceaab7faaf514f18a2472fd636dd6161ba22
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-06-03 11:01:56 +0700

    Translating Error Domain to HTTP Error

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,5 +1,7 @@
 /* eslint linebreak-style: ["error", "windows"] */
 const Hapi = require('@hapi/hapi');
+const ClientError = require('../../Commons/exceptions/ClientError');
+const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
 
 const createServer = async (container) => {
@@ -14,6 +16,42 @@ const createServer = async (container) => {
       options: { container },
     },
   ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // Mendapatkan konteks response dari request
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // bila response tersebut error, tangani sesuai kebutuhan
+      const translatedError = DomainErrorTranslator.translate(response);
+
+      // Penanganan Client Error secara Internal.
+      if (translatedError instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: translatedError.message,
+        });
+        newResponse.code(translatedError.statusCode);
+        return newResponse;
+      }
+
+      // Mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!translatedError.isServer) {
+        return h.continue;
+      }
+
+      // Penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    // Jika bukan error, lanjutkan dengan response sebelumnya ( tanpa terintervensi )
+    return h.continue;
+  });
 
   return server;
 };
