c83dc81e81d256826cbc4cd4fb6c6ca371584778
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-06-03 12:53:30 +0700

    Running HTTP Server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/* eslint linebreak-style: ["error", "windows"] */
+require('dotenv').config();
+const createServer = require('./Infrastructures/http/createServer');
+const container = require('./Infrastructures/container');
+
+const start = async () => {
+  const server = await createServer(container);
+  await server.start();
+  console.log(`server start at ${server.info.uri}`);
+};
+
+start();
